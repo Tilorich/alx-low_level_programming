49f900698f72662117c04b28cb98ae4b2f1fd3ec
Tilorich <mbarichard18@gmail.com>
2022-07-05 14:05:13 -0700

    made correction to file 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - A program that prints _putchar
  *
@@ -6,6 +6,15 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
-	return (0);
+	char name[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(name[x]);
+	}
+	_putchar('\n');
+	return 0;
+
 }
+
