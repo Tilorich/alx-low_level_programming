128c0d1f3c02cd4e3c5866ec4745aab9938e9402
Tilorich <mbarichard18@gmail.com>
2022-07-05 14:29:53 -0700

    content pushed to file1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase followed
+ * by a new line
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char x;
+
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
