a8ec321f5ced3ce47861786bf0ba1943f9614935
Tilorich <mbarichard18@gmail.com>
2022-07-06 06:31:02 -0700

    file3 added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: always 0(success)
+ */
+int _islower(int c)
+{
+	char alp;
+	int lower_alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp == c)
+		{
+			lower_alp(1);
+		}
+	}
+	Return (lower_alp);
+}
