392896967c9893e8075e56111379683d36e13736
Tilorich <mbarichard18@gmail.com>
2022-07-08 15:34:56 -0700

    changes to file9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- * main - prints fizz-Buss
+ * main - Entry point
  *
- * Return: Always 0(success)
+ * Return: Always 0
  */
 int main(void)
 {
