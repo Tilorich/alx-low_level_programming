f09f39c19b07d7cab2fc15309c11730d5a0bfb79
Tilorich <mbarichard18@gmail.com>
2022-07-08 15:16:18 -0700

    created fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 100; num++)
+	for (num = 1; num <= 100; num++)
 	{
-		if ((num / 3) && (num / 5))
+		if ((num % 3 == 0) && (num % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if(num / 5)
+		else if(num % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (num / 3)
+		else if (num % 3 == 0)
 		{
 			printf("Fizz ");
 		}
