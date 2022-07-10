a2d825710a78cefc19f6c337e0c1158b4132c331
Tilorich <mbarichard18@gmail.com>
2022-07-10 11:39:10 -0700

    changes made to file100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,13 +12,47 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x < n; x++)
+		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y < n; y++)
+			_putchar('0');
+			for (y = 0; y <= n; y++)
 			{
-				_putchar(x * y);
+				putformat(x * y);
 			}
 			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n / 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
