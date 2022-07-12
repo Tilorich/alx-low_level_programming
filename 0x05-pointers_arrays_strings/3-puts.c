80489ec7420877e8580a536039662b9889c572cf
Tilorich <mbarichard18@gmail.com>
2022-07-12 14:53:14 -0700

    file 3 added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string character
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i <= str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
