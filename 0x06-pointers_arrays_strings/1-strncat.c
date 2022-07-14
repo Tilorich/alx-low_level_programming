6085404e0d057105c073754ffe6fd656ce274d1e
Tilorich <mbarichard18@gmail.com>
2022-07-14 16:29:04 -0700

    add file 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,17 @@
  *Return: string.
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
 	j = 0;
-	while (src[j] != '\0' && j < n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
