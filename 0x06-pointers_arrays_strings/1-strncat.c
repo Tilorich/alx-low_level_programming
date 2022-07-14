737eed0d834bcd9c189cd003d488b64e0c055f3c
Tilorich <mbarichard18@gmail.com>
2022-07-14 16:09:18 -0700

    file2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,16 +4,19 @@
  *_strcat - concatinate 2 strings.
  *@dest: first string.
  *@src: second string.
+ *@n: input character
  *Return: string.
  */
 
 char *_strcat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i, j;
 
+	i = 0;
 	while (dest[i] != '\0')
 		i++;
 
+	j = 0;
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
