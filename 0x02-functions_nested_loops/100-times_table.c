11b0c76f22e2bdd829e44d35a3ad2755abb63e3a
Tilorich <mbarichard18@gmail.com>
2022-07-14 02:00:09 -0700

    updated file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,57 +1,61 @@
 #include "main.h"
 /**
- * print_times_table - prints the n time table,
- * starting with 0
- * @n: an integer input
- *
+ * print - function to print recursively using _putchar
+ * @var: variable to be printed
  * Return: Nothing
  */
-void print_times_table(int n)
+void print(int var)
 {
-	int x, y;
-
-	if (n < 0 || n > 15)
-		return;
-	for (x = 0; x <= n; x++)
+	if (var / 10)
 	{
-		_putchar('0');
-		for (y = 1; y <= n; y++)
-		{
-			putformat(x * y);
-		}
-		_putchar('\n');
+		print (var / 10);
 	}
+	_putchar('0' + (var % 10));
 }
 
 /**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
+ * print_times_table - prints the n times table, starting with 0
+ * @n: time table to print
  */
-void putformat(int n)
+void print_times_table(int n)
 {
-	if (n <= 9)
+	int i, x, y;
+
+	if (n < 0 || n > 15)
 	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
+		return;
 	}
-	else if (n < 100)
+
+	for (i = 0; i <= n; i++)
 	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n / 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
+		for (x = 0; x <= n; x++)
+		{
+			y = i * x;
+			if (y == 0)
+			{
+				_putchar('0' + y);
+			}
+			else if (y < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + y);
+			}
+			else if (y < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + y);
+			}
+			else
+			{
+				_putchar(y);
+			}
+			if (x < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
 }
