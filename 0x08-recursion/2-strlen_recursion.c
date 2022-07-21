a18128f44918b5db9f0e96729103c014aea2db4b
Tilorich <mbarichard18@gmail.com>
2022-07-21 16:00:44 -0700

    created file 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the lenght of a string
+ * @s: a string character
+ * Return: Always 0(success);
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s++) + 1);
+	}
+	else
+	{
+		return (0);
+	}
+}
