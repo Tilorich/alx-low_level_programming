97ff8bb7a3beb7524c9f56deb207361d38264c65
Tilorich <mbarichard18@gmail.com>
2022-07-21 17:13:00 -0700

    file 6 added

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
-#include "main.c"
+#include "main.h"
+int prime_con(int n, int i);
 /**
  * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
  * @n: an integer variable
