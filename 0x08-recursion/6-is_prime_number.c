c4b65ba61f574c2aff0bfa0085095f6799f63f23
Tilorich <mbarichard18@gmail.com>
2022-07-21 17:10:58 -0700

    file 6 added

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.c"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
+ * @n: an integer variable
+ * Return: 1 if prime number otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	return (prime_con(n, 2));
+}
+
+/**
+ * prime_con - checks if n is a prime
+ * @n: test case
+ * @i: recurring factors
+ * Return: 1 if n is a prime, 0 if not
+ */
+int prime_con(int n, int i)
+{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (n >= (i + 1) * (i * 1))
+	{
+		return (prime_con(n, i + 1));
+	}
+	return (1);
+}
